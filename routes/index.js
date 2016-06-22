3c8539b2828e5a47e9b3c6e981c5dafb080322c7
Lucas Barbula <lucas.barbula@gmail.com>
2016-06-22 14:28:42 -0600

    $@

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,10 @@ router.get('/', function(req, res, next) {
   })
 
 });
-
+router.get('/login', function(req, res, next){
+  res.render('login')
+})
+router.get('/signup', function(req, res, next){
+  res.render('signup')
+})
 module.exports = router;
